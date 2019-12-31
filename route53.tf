9c0daf04bbe32db5813698ced8951722881711b2
Ben Arundel <ben.arundel@cdl.co.uk>
2019-12-31 10:14:22 +0000

    fix: moving outputs and data to their own location

--- route53.tf
+++ route53.tf
@@ -1,9 +1,3 @@
-data "aws_route53_zone" "zone" {
-  provider = "aws.dns"
-  count = var.create_dns_record ? 1 : 0
-  name  = var.domain_lookup
-}
-
 resource "aws_route53_record" "dns_record" {
   provider = "aws.dns"
   count = var.create_dns_record ? 1 : 0
